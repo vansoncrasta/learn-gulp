35c7d6407c9a30cd6008ff4b99a72ecabc510f49
vansoncrasta <vansoncrasta@gmail.com>
2016-05-08 12:49:50 +0530

    JSHinit and JSCS added..

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs =  require('gulp-jscs');
 
 //Watch task
 gulp.task('default', function () {
-    console.log("Initiating Gulp..");
+    gulp.src('./src/**/*.js')
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter("jshint-stylish",{verbose:true}));
 });
+
+
